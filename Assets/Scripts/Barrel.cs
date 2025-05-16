b4eb21269fa018524307d72a71e9f6e9cb7740dd
andvigofp <aniamcion96@gmail.com>
2025-05-16 10:26:57 +0200

    Parcheado que los barriles normales, traspase los barriles de fuego, cambiando los colroles de la plataforma y escaleras level 3

--- Assets/Scripts/Barrel.cs
+++ Assets/Scripts/Barrel.cs
@@ -2,18 +2,34 @@ using UnityEngine;
 
 public class Barrel : MonoBehaviour
 {
-    private new Rigidbody2D rigidbody;
+    private Rigidbody2D rb;
     public float speed = 1f;
 
     private void Awake()
     {
-        rigidbody = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground")){
-            rigidbody.AddForce(collision.transform.right * speed, ForceMode2D.Impulse);
+        // Ignorar colisiones con barriles de fuego al instante
+        if (collision.gameObject.layer == LayerMask.NameToLayer("BarrelFireLayer"))
+        {
+            Collider2D myCollider = GetComponent<Collider2D>();
+            Collider2D otherCollider = collision.collider;
+
+            if (myCollider != null && otherCollider != null)
+            {
+                Physics2D.IgnoreCollision(myCollider, otherCollider);
+            }
+
+            return; // Salir para evitar cualquier otra lógica
+        }
+
+        // Reacción con el suelo
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            rb.AddForce(collision.transform.right * speed, ForceMode2D.Impulse);
         }
     }
 }
